964ec180cc559cd3971793b37812a8cb1f51f886
Sebsebeleb <seb@terminator.net>
2015-12-11 19:28:04 +0100

    Hello World

---
+++ Assets/Scripts/Hammer/SpinAroundController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+namespace Hammer
+{
+    using GamepadInput;
+
+    using UnityStandardAssets.Characters.FirstPerson;
+
+    public class SpinAroundController : MonoBehaviour
+    {
+
+        private HingeJoint hinge;
+
+        [SerializeField]
+        private Rigidbody hammer;
+
+        private RigidbodyFirstPersonController controller;
+
+        public float RotateSpeed;
+
+        void Awake()
+        {
+            this.hinge = this.hammer.GetComponent<HingeJoint>();
+
+            this.controller = this.GetComponentInChildren<RigidbodyFirstPersonController>();
+        }
+
+        void Update()
+        {
+
+            // Temp solution
+
+            if (GamePad.GetTrigger(GamePad.Trigger.LeftTrigger, GamePad.Index.Any) > 0.9f)
+            {
+                this.controller.movementSettings.ShouldRun = true;
+            }
+            else
+            {
+                this.controller.movementSettings.ShouldRun = false;
+            }
+
+
+            this.hinge.useSpring = false;
+            if (Input.GetButton("Fire1") || GamePad.GetTrigger(GamePad.Trigger.RightTrigger, GamePad.Index.Any) > 0.9f)
+            {
+                this.hinge.useSpring = true;
+            }
+
+            this.HandleScroll();
+        }
+
+        private void HandleScroll()
+        {
+            var d = Input.GetAxis("Mouse ScrollWheel");
+
+            d *= this.RotateSpeed;
+
+
+            var f = d * Time.deltaTime;
+
+            this.hammer.transform.Rotate(this.hammer.transform.right
+                , f);
+
+            //this.hammer.AddRelativeTorque( new Vector3(0, 0, f));
+        }
+    }
+    
+}
